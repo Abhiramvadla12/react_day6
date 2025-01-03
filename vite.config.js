a2fe32842952ee6df3328d48aa932f655da7e344
Abhiramvadla12 <abhiramvadla61@gmail.com>
2025-01-03 12:02:21 +0530

    updating......

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react_day6/', // Match your repository name
+  // base: '/react_day6/', // Match your repository name
   // build: {
   //   outDir: 'dist', // Default output directory for Vite
   // },
